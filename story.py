627339cca3e4fed18ee1a7811c302c30e2ba4fc9
In Young Song <231744384+In-Young-Song@users.noreply.github.com>
2025-09-18 18:50:21 +0000

    Villain path: final corrupted ending.

--- story.py
+++ story.py
@@ -14,6 +14,6 @@ def left_path():
 def right_path():
     print("You walk right and encounter a talking squirrel who challenges you to a duel.")
     print("You accept the curse and become the forest’s new dark overlord!")
-
+    print("Your shadow spreads across the forest as all creatures kneel in fear.")
 if __name__ == "__main__":
     intro()
